b20b1994a26eb52448c9e1f72f83a750b4fc9ecf
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-26 23:37:18 +0100

    Add filterable list and social media handle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
+import { search, form, refreshBtn } from './modules/variables.js';
 import handleSubmit from './modules/form.js';
-import { form, refreshBtn } from './modules/variables.js';
 import displayScore from './modules/async.js';
+import { filter } from './modules/fetch.js';
 
 refreshBtn.addEventListener('click', displayScore);
 form.addEventListener('submit', handleSubmit);
+search.addEventListener('keyup', filter);
