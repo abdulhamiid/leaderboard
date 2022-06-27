2f974f4c04d4cc5519ecd88bb6f70383cabb6c0c
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-27 09:36:04 +0100

    Add functionality to filter names

--- src/modules/variables.js
+++ src/modules/variables.js
@@ -2,10 +2,11 @@ const scorePlaceholder = document.querySelector('#scores');
 const form = document.querySelector('#form');
 const name = document.querySelector('input[type="text"]');
 const score = document.querySelector('input[type="number"]');
+const search = document.querySelector('#search');
 const refreshBtn = document.querySelector('#refresh');
 const resultCount = document.querySelector('#result');
 const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/JUUg16quXPBEL9Inuyon/scores/';
 
 export {
-  scorePlaceholder, form, name, score, refreshBtn, baseUrl, resultCount,
+  scorePlaceholder, form, name, score, refreshBtn, baseUrl, resultCount, search,
 };
