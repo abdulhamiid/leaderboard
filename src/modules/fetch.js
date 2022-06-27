20ce560be4c38a380637ac02059c3851d5c1c49a
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-27 09:31:43 +0100

    Add function to filter search

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -1,4 +1,8 @@
-import { baseUrl, scorePlaceholder, resultCount } from './variables.js';
+import {
+  baseUrl, scorePlaceholder, resultCount, search,
+} from './variables.js';
+
+let names = [];
 
 const fetchData = () => {
   scorePlaceholder.innerHTML = '';
@@ -10,15 +14,28 @@ const fetchData = () => {
       <li class="score" >${data.user}: ${data.score}</li>`;
         document.querySelectorAll('.score').forEach((li) => {
           const R = Math.floor(Math.random() * 255);
-          const G = Math.floor(Math.random() * 255);
-          const B = Math.floor(Math.random() * 255);
+          const G = Math.floor(Math.random() * 244);
+          const B = Math.floor(Math.random() * 100);
           const bg = `rgb(${R}, ${G}, ${B})`;
           li.style.backgroundColor = bg;
         });
       });
+      names = scorePlaceholder.querySelectorAll('li');
       resultCount.textContent = `(${data.result.length}) Players`;
     });
 };
+
+const filter = () => {
+  const filterValue = search.value.toUpperCase();
+  for (let i = 0; i < names.length; i += 1) {
+    if (names[i].innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      names[i].style.display = '';
+    } else {
+      names[i].style.display = 'none';
+    }
+  }
+};
+
 fetchData();
 
-export default fetchData;
+export { fetchData, filter };
